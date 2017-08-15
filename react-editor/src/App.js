487c261ef943b2d54e419f0fd843c9351d7476cc
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2017-08-15 10:53:02 -0700

    add font size functionality

--- react-editor/src/App.js
+++ react-editor/src/App.js
@@ -1,18 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const shortHeader = <h2>My Short Message</h2>;
+const longHeader = <h1>My Message</h1>;
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: props.initialMessage,
+      color: '#4682b4',
+      fontSize: 18
+    }
+  }
+
+  handleChange({ name, value }) {
+    this.setState({ 
+      [name]: value
+    });
+  }
+
   render() {
+    const header = this.state.message.length < 20 ? shortHeader : longHeader;
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        {header}
+        <div>
+
+          <label>
+            Message:
+            <input name="message" value={this.state.message}
+              onChange={({target}) => this.handleChange(target)}/>
+          </label>
+
+          <label>
+            Color:
+            <input name="color" value={this.state.color} type="color"
+              onChange={({target}) => this.handleChange(target)}/>
+          </label>
+
+          <label>
+            Font Size:
+            <input name="fontSize" value={this.state.size} type="fontSize"
+              onChange={({target}) => this.handleChange(target)}/>
+          </label>
+
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="display" style={{
+          color: this.state.color,
+          fontSize: this.state.fontSize
+        }}>
+          {this.state.message}
+        </div>
+
       </div>
     );
   }
--- react-editor/src/index.js
+++ react-editor/src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App initialMessage="Hey dude" />, document.getElementById('root'));
 registerServiceWorker();
